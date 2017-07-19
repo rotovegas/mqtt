f32daa12fe6fa378f8c710854ac8bbdb1db32f05
Stephen Agyiri Akuffo <stivinrotovegas@gmail.com>
2017-07-19 11:36:09 +0000

    mqtt_flash_led

---
+++ light_message.py
@@ -0,0 +1,43 @@
+import paho.mqtt.client as mqtt
+from gpiozero import LED
+from time import sleep
+led = LED(17)
+
+
+def dot():
+        led.on()
+        sleep(1)
+        led.off()
+        sleep(1)
+
+def dash():
+	led.on()
+	sleep(0.4)
+	led.off()
+	sleep(0.5)
+
+
+
+def on_message(client, userdata, message):
+        print message.payload
+	dot()
+	dash()
+	dot()
+	dot()
+def on_connect(client, userdata, flags, code):
+        print "connected:" + str(code)
+        client.subscribe("test/all")
+
+
+client = mqtt.Client()
+client.on_connect = on_connect
+client.on_message = on_message
+
+client.connect("moorhouseassociates.com", 1883, 60)
+
+
+client.loop_forever()
+
+
+
+
